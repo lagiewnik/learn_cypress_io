1a3851ffb84cb25d14ef8fdbcf76f579b00ae329
lagiewnik <lagiewnik@gmail.com>
2022-07-31 21:35:16 +0200

    first lesson https://learn.cypress.io/testing-your-first-application/installing-cypress-and-writing-your-first-test

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,18 @@
+describe('empty spec', () => {
+  beforeEach(()=>{
+    cy.visit('http://localhost:3000')
+  }
+  )
+  
+  it('passes', () => {
+    
+    cy.get('[data-test="hero-heading"]').contains("Testing Next.js Applications with Cypress")
+  })
+
+  it('Features on homepage are correct', () => {
+    
+    cy.get("dt").eq(0).contains("4 Courses")
+    cy.get("dt").eq(1).contains("25+ Lessons")
+    cy.get("dt").eq(2).contains("Free and Open Source")
+  });
+})
